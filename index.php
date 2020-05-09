6736f8a844a51ea56892d11ef83bea0f1ca5381e
TimVance <2DngxvshA6GUGcQ>
2020-05-09 21:36:06 +0300

    Секция обратная связь

--- index.php
+++ index.php
@@ -141,21 +141,74 @@ $APPLICATION->SetTitle('Главная');
 <div class="container">
     <div class="row align-items-center contact-form">
         <div class="col-lg-7 col-md-12">
-            <p>Мы с Вами свяжемся и ответим на любые вопросы</p>
+            <?$APPLICATION->IncludeComponent(
+                "bitrix:main.include",
+                "",
+                Array(
+                    "AREA_FILE_SHOW" => "file",
+                    "AREA_FILE_SUFFIX" => "inc",
+                    "EDIT_TEMPLATE" => "",
+                    "PATH" => SITE_TEMPLATE_PATH."/include/parts/feedback_title.php"
+                )
+            );?>
         </div>
         <div class="col-lg-5 col-md-12">
-            <form class="form">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Ваше имя">
-                </div>
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Номер телефона">
-                </div>
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Ваш e-email">
-                </div>
-                <button type="submit" class="btn">Отправить</button>
-            </form>
+            <?$APPLICATION->IncludeComponent(
+                "altasib:feedback.form",
+                "feedback_section",
+                Array(
+                    "ACTIVE_ELEMENT" => "Y",
+                    "ADD_EVENT_FILES" => "N",
+                    "ADD_HREF_LINK" => "Y",
+                    "ADD_LEAD" => "N",
+                    "ALX_LINK_POPUP" => "N",
+                    "BBC_MAIL" => "",
+                    "CAPTCHA_TYPE" => "default",
+                    "CATEGORY_SELECT_NAME" => "Выберите категорию",
+                    "CHANGE_CAPTCHA" => "N",
+                    "CHECKBOX_TYPE" => "CHECKBOX",
+                    "CHECK_ERROR" => "Y",
+                    "COLOR_OTHER" => "#009688",
+                    "COLOR_SCHEME" => "BRIGHT",
+                    "COLOR_THEME" => "",
+                    "EVENT_TYPE" => "ALX_FEEDBACK_FORM",
+                    "FB_TEXT_NAME" => "",
+                    "FB_TEXT_SOURCE" => "PREVIEW_TEXT",
+                    "FORM_ID" => "2",
+                    "IBLOCK_ID" => "3",
+                    "IBLOCK_TYPE" => "altasib_feedback",
+                    "INPUT_APPEARENCE" => array("DEFAULT"),
+                    "JQUERY_EN" => "N",
+                    "LINK_SEND_MORE_TEXT" => "Отправить ещё одно сообщение",
+                    "LOCAL_REDIRECT_ENABLE" => "N",
+                    "MASKED_INPUT_PHONE" => array("PHONE"),
+                    "MESSAGE_OK" => "Ваше сообщение было успешно отправлено",
+                    "NAME_ELEMENT" => "PHONE",
+                    "NOT_CAPTCHA_AUTH" => "Y",
+                    "PROPERTY_FIELDS" => array("PHONE","FIO","EMAIL"),
+                    "PROPERTY_FIELDS_REQUIRED" => array("PHONE","FIO","EMAIL"),
+                    "PROPS_AUTOCOMPLETE_EMAIL" => array("EMAIL"),
+                    "PROPS_AUTOCOMPLETE_NAME" => array("FIO"),
+                    "PROPS_AUTOCOMPLETE_PERSONAL_PHONE" => array("PHONE"),
+                    "PROPS_AUTOCOMPLETE_VETO" => "N",
+                    "REQUIRED_SECTION" => "N",
+                    "SECTION_FIELDS_ENABLE" => "N",
+                    "SECTION_MAIL_ALL" => "timvance@ya.ru",
+                    "SEND_IMMEDIATE" => "N",
+                    "SEND_MAIL" => "N",
+                    "SHOW_LINK_TO_SEND_MORE" => "N",
+                    "SHOW_MESSAGE_LINK" => "N",
+                    "SPEC_CHAR" => "N",
+                    "USERMAIL_FROM" => "N",
+                    "USER_CONSENT" => "N",
+                    "USER_CONSENT_ID" => "1",
+                    "USER_CONSENT_INPUT_LABEL" => "Согласие на обработку персональных данных",
+                    "USER_CONSENT_IS_CHECKED" => "Y",
+                    "USER_CONSENT_IS_LOADED" => "N",
+                    "USE_CAPTCHA" => "N",
+                    "WIDTH_FORM" => ""
+                )
+            );?>
         </div>
     </div>
 </div>
